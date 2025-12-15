5c5558221c38393794f4fb72bd5121ba81c7e4e9
sabrilamouchi <lamouchsabri@gmail.com>
2025-12-15 10:02:25 +0100

    Set Vite base to /sabri_portfolio/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),],
-  base:'/sabri-portfolio/',
+  base:'/sabri_portfolio/',
     resolve: {
     alias: {
       // eslint-disable-next-line no-undef
