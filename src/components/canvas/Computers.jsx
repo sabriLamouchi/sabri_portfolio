d73ac631d6fdb9616eb98df5de405a55949de0fd
sabrilamouchi <lamouchsabri@gmail.com>
2025-12-15 10:22:31 +0100

    ast changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import { OrbitControls,useGLTF } from '@react-three/drei'
 // import CanvasLoader from'../Loader'
 const Computers = ({isMobile}) => {
 
-  const computer=useGLTF('desktop_pc/scene.gltf');
+  const computer=useGLTF(`${import.meta.env.BASE_URL}desktop_pc/scene.gltf`);
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
@@ -18,7 +18,7 @@ const Computers = ({isMobile}) => {
       object={computer.scene}
       scale={isMobile ? 0.7:0.75}
       position={ isMobile ? [0,-3,-2.2] : [0,-3.25,-1.5]}
-      rotaion={[-0.01,-0.2,-0.1]}
+      rotation={[-0.01,-0.2,-0.1]}
         />
     </mesh>
   )
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,7 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
 import Loader from '../Loader'
 const Earth = () => {
-  const earth=useGLTF('./planet/scene.gltf')
+  const earth=useGLTF(`${import.meta.env.BASE_URL}planet/scene.gltf`)
   return (
     <primitive
     object={earth.scene}
